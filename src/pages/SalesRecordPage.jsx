1b9530a55bdbde325e4ce9820aaf823d625aed89
francoosman <francoosman12@gmail.com>
2024-08-13 12:44:42 -0300

    cambio en mis rutas para ventas

--- src/pages/SalesRecordPage.jsx
+++ src/pages/SalesRecordPage.jsx
@@ -10,7 +10,7 @@ const SalesRecordPage = () => {
   useEffect(() => {
     const fetchSales = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/sales/ids`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/sales`);
         setSales(response.data);
       } catch (err) {
         setError('Error fetching sales data');
