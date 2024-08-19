94b3be1004d3d3ceb5ca7f623ce0a730bf1c77af
francoosman <francoosman12@gmail.com>
2024-08-19 09:13:49 -0300

    Cambios en los estilos de Pagina de Registros

--- src/pages/SalesRecordPage.jsx
+++ src/pages/SalesRecordPage.jsx
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../styles/SalesRecordPage.css'; // Asegúrate de importar el CSS
+import '../styles/SalesRecordPage.css';
 
 const SalesRecordPage = () => {
   const [sales, setSales] = useState([]);
   const [error, setError] = useState('');
   const [totalAmount, setTotalAmount] = useState(0);
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
 
   useEffect(() => {
     const fetchSales = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/sales`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/sales`, {
+          params: {
+            startDate,
+            endDate,
+          },
+        });
         const salesData = response.data;
         setSales(salesData);
 
-        // Calcular el total de ventas
         const total = salesData.reduce((acc, sale) => acc + (sale.price * sale.quantity), 0);
         setTotalAmount(total);
 
@@ -25,12 +31,41 @@ const SalesRecordPage = () => {
     };
 
     fetchSales();
-  }, []);
+  }, [startDate, endDate]);
+
+  const handleFilterSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para manejar el envío del filtro si se necesita
+  };
 
   return (
     <div className="sales-record-page">
       <h1 className="page-title">Registro de Ventas</h1>
+
+      <div className="filter-container">
+        <div>
+          <label>Fecha de Inicio:</label>
+          <input 
+            type="date" 
+            value={startDate} 
+            onChange={(e) => setStartDate(e.target.value)} 
+          />
+        </div>
+        <div>
+          <label>Fecha de Fin:</label>
+          <input 
+            type="date" 
+            value={endDate} 
+            onChange={(e) => setEndDate(e.target.value)} 
+          />
+        </div>
+        <div>
+          <button onClick={handleFilterSubmit} className='boton-filtrar'>Filtrar</button>
+        </div>
+      </div>
+
       {error && <p className="error-message">{error}</p>}
+
       <table className="sales-record-table">
         <thead>
           <tr>
