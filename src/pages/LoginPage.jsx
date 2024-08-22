442d47d27ac4410478e98ccc028e6c6f5cd54f17
francoosman <francoosman12@gmail.com>
2024-08-22 08:58:53 -0300

    cambios en el login para renderizar el registro y cambios en el estilo

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,5 @@
-// src/pages/LoginPage.jsx
-
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import '../styles/LoginPage.css';
 import { useAuth } from '../context/AuthContext';
 
@@ -13,19 +10,19 @@ const LoginPage = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [name, setName] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate();
+  const [successMessage, setSuccessMessage] = useState('');
   const { login } = useAuth();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     if (isLogin) {
       try {
         const response = await axios.post('http://localhost:5000/api/auth/login', { email, password });
         console.log('Inicio de sesión exitoso:', response.data);
         const { token, role, name } = response.data;
-        login(token, role, name); // Pasa el nombre del usuario a la función de login
-        navigate('/sales');
+        login(token, role, name);
+        // Aquí puedes redirigir o hacer cualquier otra acción
       } catch (error) {
         setError(error.response ? error.response.data : 'Error al iniciar sesión');
         console.error('Error al iniciar sesión:', error.response ? error.response.data : error.message);
@@ -35,24 +32,32 @@ const LoginPage = () => {
         setError('Las contraseñas no coinciden');
         return;
       }
-  
+
       try {
         const response = await axios.post('http://localhost:5000/api/auth/register', { name, email, password });
         console.log('Registro exitoso:', response.data);
-        navigate('/login');
+        setSuccessMessage('Registro exitoso. Redirigiendo al inicio de sesión...');
+        setError('');
+
+        // Espera 3 segundos y luego cambia a la vista de login
+        setTimeout(() => {
+          setIsLogin(true);
+          setSuccessMessage('');
+        }, 3000);
       } catch (error) {
         setError(error.response ? error.response.data : 'Error al registrarse');
         console.error('Error al registrarse:', error.response ? error.response.data : error.message);
       }
     }
   };
-  
+
   return (
     <div className="auth-container">
       <div className="auth-form-panel">
         <h2>{isLogin ? 'Iniciar Sesión' : 'Registrarse'}</h2>
         <form onSubmit={handleSubmit}>
           {error && <div className="error-message">{error}</div>}
+          {successMessage && <div className="success-message">{successMessage}</div>}
           {!isLogin && (
             <input
               type="text"
@@ -86,11 +91,14 @@ const LoginPage = () => {
             />
           )}
           <button type="submit">{isLogin ? 'Ingresar' : 'Registrarse'}</button>
-          {!isLogin && <a href="#">¿Olvidaste tu contraseña?</a>}
         </form>
         <button
           className="toggle-button"
-          onClick={() => setIsLogin(!isLogin)}
+          onClick={() => {
+            setIsLogin(!isLogin);
+            setError('');
+            setSuccessMessage('');
+          }}
         >
           {isLogin ? '¿No tienes cuenta? Regístrate aquí' : '¿Ya tienes cuenta? Inicia sesión'}
         </button>
