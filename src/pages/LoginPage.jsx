dda2f4417621724c093e739f8df86013f5c27a94
Franco Osman <francoosman12@gmail.com>
2024-09-04 11:07:40 -0300

    cambios para que funsione el login/register

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -24,8 +24,12 @@ const LoginPage = () => {
         login(token, role, name);
         // Aquí puedes redirigir o hacer cualquier otra acción
       } catch (error) {
-        setError(error.response ? error.response.data : 'Error al iniciar sesión');
-        console.error('Error al iniciar sesión:', error.response ? error.response.data : error.message);
+        // Asegúrate de que error.response.data sea una cadena o manipúlalo adecuadamente
+        const errorMessage = error.response && typeof error.response.data === 'string'
+          ? error.response.data
+          : 'Error al iniciar sesión';
+        setError(errorMessage);
+        console.error('Error al iniciar sesión:', errorMessage);
       }
     } else {
       if (password !== confirmPassword) {
@@ -45,8 +49,12 @@ const LoginPage = () => {
           setSuccessMessage('');
         }, 3000);
       } catch (error) {
-        setError(error.response ? error.response.data : 'Error al registrarse');
-        console.error('Error al registrarse:', error.response ? error.response.data : error.message);
+        // Asegúrate de que error.response.data sea una cadena o manipúlalo adecuadamente
+        const errorMessage = error.response && typeof error.response.data === 'string'
+          ? error.response.data
+          : 'Error al registrarse';
+        setError(errorMessage);
+        console.error('Error al registrarse:', errorMessage);
       }
     }
   };
